20ad0eb7fabe500114398d10faa79df8a28e7837
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-25 20:52:04 -0600

    Add documentation

--- cf/validate.sh
+++ cf/validate.sh
@@ -1,2 +1,4 @@
+# https://docs.aws.amazon.com/cli/latest/reference/cloudformation/validate-template.html
+
 $ aws cloudformation validate-template --template-url file://example.json
 $ aws cloudformation validate-template --template-body file://example.json
