66a50f212a170ac078fd4ff9121d099401c3813b
aa-ag <aaron.aguerrevere@gmail.com>
2023-02-27 11:01:14 -0600

    Add full docs & mention to optionality

--- cf/validate.sh
+++ cf/validate.sh
@@ -1,4 +1,11 @@
+## References
+# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/best-practices.html
 # https://docs.aws.amazon.com/cli/latest/reference/cloudformation/validate-template.html
+# https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-validate-template.html
+# https://aws.amazon.com/premiumsupport/knowledge-center/cloudformation-template-validation/
 
 $ aws cloudformation validate-template --template-url file://example.json
+
+# or 
+
 $ aws cloudformation validate-template --template-body file://example.json
