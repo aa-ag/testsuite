658a1b4eb6b0ff4429ed760615c3b6cbb72926bc
aa-ag <aaron.aguerrevere@gmail.com>
2023-03-03 17:15:47 -0600

    Add test to check first if template is where it should be

--- cf/validate.py
+++ cf/validate.py
@@ -2,6 +2,7 @@
 import unittest
 import requests
 import subprocess
+import os
 
 ############------------ GLOBAL VARIABLE(S) ------------#######################
 template = ''
@@ -14,19 +15,22 @@ class TestCloudFormation(unittest.TestCase):
         - test launch form launches what is expected
         - test infra is resourced as expected
     '''
-    def test_cf_template_location(self):
-        r = requests.get(template)
-        self.assertEqual(r.status_code,200)
-    
-    def test_cf_template_url_validity(self):
-        validation_command = f'aws cloudformation validate-template --template-url {template}'
-        r = subprocess.run(validation_command, shell=True)
-        self.assertNotIn('error',r)
+    def test_cf_template_existence(self):
+        pass
 
-    def test_cf_template_body_validity(self):
-        validation_command = f'aws cloudformation validate-template --template-body {template}'
-        r = subprocess.run(validation_command, shell=True)
-        self.assertNotIn('error',r)
+    # def test_cf_template_location(self):
+    #     r = requests.get(template)
+    #     self.assertEqual(r.status_code,200)
+    
+    # def test_cf_template_url_validity(self):
+    #     validation_command = f'aws cloudformation validate-template --template-url {template}'
+    #     r = subprocess.run(validation_command, shell=True)
+    #     self.assertNotIn('error',r)
+
+    # def test_cf_template_body_validity(self):
+    #     validation_command = f'aws cloudformation validate-template --template-body {template}'
+    #     r = subprocess.run(validation_command, shell=True)
+    #     self.assertNotIn('error',r)
 
 
 ############------------ DRIVER CODE ------------##############################ß
